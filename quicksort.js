6755c04e724873f5cb8777f0f477ed49dd375dc5
Lucas Martinez Asensio <martinezasensiolucas@gmail.com>
2024-01-24 19:45:02 -0300

    fullfill Array con randoms

--- quicksort.js
+++ quicksort.js
@@ -1,34 +1,43 @@
 function quicksort(array) {
-    if (array.length <= 1) {
-      return array;
-    }
-
-    const pivot = array[0];
-
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < array.length; i++) {
-      if (array[i][0] < pivot[0]) {
-        left.push(array[i]);
-      } else {
-        right.push(array[i]);
-      }
-    }
-
-    return quicksort(left).concat([pivot], quicksort(right));
-  }
-  let vectorDesord = [];
-  
-  for (let i = 0; i < 5; i++) {
-    vectorDesord[i] = new Array(2);
-    vectorDesord[i][0] = prompt("Ingresa un entero");
-    vectorDesord[i][1] = i + 1;
+  if (array.length <= 1) {
+    return array;
   }
 
-  vectorOrd = vectorDesord.map(vectorElement=>vectorElement.slice());
+  const pivot = array[0];
 
-  vectorOrd = quicksort(vectorOrd);
+  const left = [];
+  const right = [];
 
-  console.log("Vector Ordenado:", vectorOrd);
-  console.log("Vector Desordenado:", vectorDesord);
+  for (let i = 1; i < array.length; i++) {
+    if (array[i][0] < pivot[0]) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return quicksort(left).concat([pivot], quicksort(right));
+}
+const vectorLenght = 1000000;
+let vectorDesord = new Array(vectorLenght);
+
+
+for (let i = 0; i < vectorDesord.length; i++) {
+  vectorDesord[i] = new Array(2);
+  vectorDesord[i][0] = Math.floor(Math.random() * vectorLenght);
+  vectorDesord[i][1] = i + 1;
+}
+
+let vectorOrd = vectorDesord.map(vectorElement=>vectorElement.slice());
+let vectorOrd2 = vectorDesord.map(vectorElement=>vectorElement.slice());
+
+console.time();
+vectorOrd = quicksort(vectorOrd);
+console.timeEnd();
+
+console.time();
+vectorOrd2 = vectorOrd2.sort();
+console.timeEnd();
+
+console.log("Vector Ordenado:", vectorOrd);
+console.log("Vector Desordenado:", vectorDesord);
