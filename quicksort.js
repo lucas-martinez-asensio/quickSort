d391ecd8e4d028b029c0dd2e56714cca123a353a
Lucas Martinez Asensio <martinezasensiolucas@gmail.com>
2024-01-25 18:25:34 -0300

    llamado .sort() mediante callback Ln 41

--- quicksort.js
+++ quicksort.js
@@ -18,16 +18,6 @@ function quicksort(array) {
 
   return quicksort(left).concat([pivot], quicksort(right));
 }
-<<<<<<< HEAD
-
-const vectorLenght = 1000000;
-let vectorDesord = new Array(vectorLenght);
-
-for (let i = 0; i < vectorDesord.length; i++) {
-  vectorDesord[i] = new Array(2);
-  vectorDesord[i][0] = Math.floor(Math.random() * vectorLenght);
-  vectorDesord[i][1] = i + 1;
-=======
 
 const vectorLength = 1000000;
 let vectorMessy = new Array(vectorLength);
@@ -36,7 +26,6 @@ for (let i = 0; i < vectorMessy.length; i++) {
   vectorMessy[i] = new Array(2);
   vectorMessy[i][0] = Math.floor(Math.random() * vectorLength);
   vectorMessy[i][1] = i + 1;
->>>>>>> 6c09684aef6e7a169c690b6bd9fc320208b01fe6
 }
 
 let vectorOrdered = vectorMessy.map(vectorElement=>vectorElement.slice());
@@ -49,10 +38,5 @@ console.timeEnd();
 
 //tiempo de ejecución ordenamiento nativo
 console.time();
-<<<<<<< HEAD
 vectorOrdered2 = vectorOrdered2.sort(([a], [z]) => a - z);
 console.timeEnd();
-=======
-vectorOrdered2 = vectorOrdered2.sort();
-console.timeEnd();
->>>>>>> 6c09684aef6e7a169c690b6bd9fc320208b01fe6
